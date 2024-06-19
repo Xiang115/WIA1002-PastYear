bfd96d26589cc71f6f4ed60929c73468cebe5b7d
SkibidiToiletPro <149899139+JaySee04@users.noreply.github.com>
2024-06-19 19:34:37 +0800

    Update Q3.java and Hoong Liang Smol PP

--- JaySee/DS Past Year 2017 S1/Q3/src/Q3.java
+++ JaySee/DS Past Year 2017 S1/Q3/src/Q3.java
@@ -6,8 +6,12 @@ public class Q3{
         System.out.println("Enter a word to be reversed:");
         String input = sc.nextLine();
         String reversed = reverseWord(input);
-        System.out.println("Original word: " + input);
-        System.out.println("Reversed word: " + reversed);
+        if(!reversed.isEmpty()){
+            System.out.println("Original word: " + input);
+            System.out.println("Reversed word: " + reversed);
+        }else{
+            System.out.println("Walao why you enter empty string?");
+        }
         sc.close();
     }
 
@@ -25,5 +29,51 @@ public class Q3{
         }
 
         return reversed.toString();
+    }
+}
+
+class DIYStack {
+    private int max;
+    private char[] stackArray;
+    private int top;
+
+    public DIYStack(int max) {
+        this.max = max;
+        stackArray = new char[max];
+        top = -1;
+    }
+
+    public void push(char c) {
+        if(isStackFull()){
+            System.out.println("Stack full liao, kenot push " + c);
+        }else{
+            stackArray[++top] = c;
+        }
+    }
+
+    public char pop() {
+        if(isStackEmpty()){
+            System.out.println("Stack empty leh, no popping");
+            return '\0';
+        }else{
+            return stackArray[top--];
+        }
+    }
+
+    public char peek() {
+        if(isStackEmpty()){
+            System.out.println("Stack empty leh, why you peek peek harr");
+            return '\0';
+        }else{
+            return stackArray[top];
+        }
+    }
+
+    public boolean isStackEmpty() {
+        return top == -1;
+    }
+
+    public boolean isStackFull() {
+        return top == max - 1;
     }
 }
